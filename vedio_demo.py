6f6a65774056350de79e66728634c30324a84a7d
primarypit <2895123212@qq.com>
2024-04-01 14:34:29 +0800

    update

--- vedio_demo.py
+++ vedio_demo.py
@@ -3,29 +3,28 @@ from VecRetrieval import *
 from LLMAPI import *
 from langchain import OpenAI
 import warnings
+import re
 warnings.filterwarnings('ignore')
 
-open_key="****"
+open_key="***"
 llmai = OpenAI(temperature=0, openai_api_key=open_key)
 
-file_folder_dir = "Docs/"
 saving_dir = "FAISS_store/"
-file_name = "Black_Cat.pdf"
+url = "https://www.youtube.com/watch?v=zIwLWfaAg-8"
 
-#doc = Document(file_folder_dir, file_name)
-#doc.summary(llmai)
-#doc_name = doc.get_name()
-doc_name = "Black_Cat"
-summary = load_summary(doc_name)
+video = Video(url)
+video.summary(llmai)
+video_name = video.get_name()
+summary = load_summary(video_name)
 
 search_type = "similarity"
 search_kwargs = {"k": 10}
 
 vecs = VecStore(search_type, search_kwargs)
-#vecs.build_retriever(doc, True)
-vecs.load_retriever(saving_dir, doc_name)
+vecs.build_retriever(video, True)
+vecs.load_retriever(saving_dir, video_name)
 
-test_query = "How many cats appeared in the story"
+test_query = "What kind of future does Elon Musk mention"
 
 related_contents = vecs.query_search(test_query)
 related_contents = sorted(related_contents, key = lambda x: x.metadata["order"])
